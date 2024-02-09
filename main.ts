7008b2ef68bb6c2b3b5824586248f7869f877935
junnienn <159075257+junnienn@users.noreply.github.com>
2024-02-09 09:10:56 -0800

    updated tilemap

--- main.ts
+++ main.ts
@@ -1,10 +1,28 @@
-
 info.player1.setLife(3)
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function(sprite: Sprite, location: tiles.Location) {
+    tiles.placeOnTile(MainPlayer, tiles.getTileLocation(5, 122))
     info.changeLifeBy(-1)
     
 })
-
+let MainPlayer: Sprite = null
+MainPlayer = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
 function Player_y (jump: boolean) {
     sy += Gravity
     if (MainPlayer.isHittingTile(CollisionDirection.Bottom)) {
@@ -67,26 +85,7 @@ let Friction = 0
 let Aceleration = 0
 let sy = 0
 let sx = 0
-let MainPlayer: Sprite = null
-MainPlayer = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-tiles.placeOnTile(MainPlayer, tiles.getTileLocation(1, 5))
+
 sx = 0
 sy = 0
 Aceleration = 10
